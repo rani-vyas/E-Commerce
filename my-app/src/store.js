095a83a9755621542823fd22406857aa88da94c5
--rani-vyas <--ranivyas2003@gmail.com>
2023-12-04 17:10:25 +0530

    change in store.js file

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import categorySlice from "./slice/categorySlice";
 import cartSlice from "./slice/cartSlice";
 import productSlice from "./slice/productSlice";
